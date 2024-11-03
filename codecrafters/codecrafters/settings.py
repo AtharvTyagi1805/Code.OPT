bf91871984056265bc3f82f7b1485edefcc32bc3
unknown <tatharv597@gmail.com>
2024-11-03 22:24:07 +0530

    Initial commit

--- codecrafters/codecrafters/settings.py
+++ codecrafters/codecrafters/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.runserver_nostatic"
+    "django.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
